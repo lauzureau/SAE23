be829c80487d09cf888156d2b5815cca54d88590
lauzureau <133368444+lauzureau@users.noreply.github.com>
2023-05-23 11:58:09 +0200

    Update About.php

--- About.php
+++ About.php
@@ -9,17 +9,18 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Qui sommes-nous ?</title>
 </head>
-<body>
+<body style="background-color:Gray;">
+<img src="./img/Mondial_NFT_Saler_2_1.png" class="img-thumbnail rounded mx-auto d-block" alt="logo">
 <div class="container mt-3">
-  <h2>Qui sommes-nous ?</h2>
-  <div class="mt-4 p-5 bg-primary text-white rounded">
+  <h2 style="color:#ffc107;">Qui sommes-nous ?</h2>
+  <div class="mt-4 p-5 bg-dark text-warning rounded">
     <h1>Qui est NTFExchange ?</h1> 
     <p>NTFExchange est une entreprise travaillant dans le domaine des NFT (Non-Fungible Tokens) et de l'échange de biens numériques uniques. Nous sommes une plateforme en ligne sécurisée qui permet aux utilisateurs d'acheter, de vendre et de échanger des NFT de manière transparente et efficace.</p> 
   </div>
 </div><div class="container mt-3">
-  <div class="mt-4 p-5 bg-primary text-white rounded">
+  <div class="mt-4 p-5 bg-dark text-warning rounded">
     <h1>Qui travail chez NTFExchange ?</h1> 
-    <p>NTFExchange est géré par principalement par 4 personnes</p> 
+    <p>NTFExchange est géré par principalement par 4 personnes : </br>- Auzureau Loïck</br>- Granier-Nallet Mat-Téo</br>- Manivel Hugo</br>- Raso Gwénaëlle</p> 
   </div>
 </div>
 </body>
