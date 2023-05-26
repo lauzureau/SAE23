d8b4ef366bfcdd084b50caba20c81c3c67c694a9
lauzureau <133368444+lauzureau@users.noreply.github.com>
2023-05-26 14:41:53 +0200

    Update About.php

--- About.php
+++ About.php
@@ -1,27 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Qui sommes-nous ?</title>
-</head>
-<body style="background-color:Gray;">
-<img src="./img/Mondial_NFT_Saler_2_1.png" class="img-thumbnail rounded mx-auto d-block" alt="logo">
-<div class="container mt-3">
-  <h2 style="color:#ffc107;">Qui sommes-nous ?</h2>
-  <div class="mt-4 p-5 bg-dark text-warning rounded">
-    <h1>Qui est NTFExchange ?</h1> 
-    <p>NTFExchange est une entreprise travaillant dans le domaine des NFT (Non-Fungible Tokens) et de l'échange de biens numériques uniques. Nous sommes une plateforme en ligne sécurisée qui permet aux utilisateurs d'acheter, de vendre et de échanger des NFT de manière transparente et efficace.</p> 
-  </div>
-</div><div class="container mt-3">
-  <div class="mt-4 p-5 bg-dark text-warning rounded">
-    <h1>Qui travail chez NTFExchange ?</h1> 
-    <p>NTFExchange est géré par principalement par 4 personnes : </br>- Auzureau Loïck</br>- Granier-Nallet Mat-Téo</br>- Manivel Hugo</br>- Raso Gwénaëlle</p> 
-  </div>
+<?php
+
+include "./functions.php";
+
+setup('Qui sommes nous ?');
+
+echo ' <div class="container mt-3">';
+
+pagenavbar();
+
+pageheader('Qui sommes nous ?');
+
+echo '<div class="mt-4 p-5 bg-dark text-warning rounded">
+<h1>Qui est NTFExchange ?</h1> 
+<p>NTFExchange est une entreprise travaillant dans le domaine des NFT (Non-Fungible Tokens) et de l échange de biens numériques uniques. Nous sommes une plateforme en ligne sécurisée qui permet aux utilisateurs d acheter, de vendre et d échanger des NFT de manière transparente et efficace.</p> 
 </div>
-</body>
-</html>
+</div><div class="container mt-3">
+<div class="mt-4 p-5 bg-dark text-warning rounded">
+<h1>Qui travail chez NTFExchange ?</h1> 
+<p>NTFExchange est géré par principalement par 4 personnes : </br>- Auzureau Loïck</br>- Granier-Nallet Mat-Téo</br>- Manivel Hugo</br>- Raso Gwénaëlle</p> 
+</div>';
+
+pagefooter();
+
+?>
