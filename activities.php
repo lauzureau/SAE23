ae26c00912a4da30f4fb4643e60f8437b189a1de
MGranierNallet <133660383+MGranierNallet@users.noreply.github.com>
2023-05-30 16:21:18 +0200

    Add files via upload

---
+++ activities.php
@@ -0,0 +1,36 @@
+<?php
+
+include "functions.php";
+
+setup("SAE23");
+
+
+
+pageheader('NTFExchange');
+
+pagenavbar();
+
+echo "
+	<div class='container'>
+		<h1>Nos activités</h1>
+		<ul> 
+		<li>Plateforme d'échange de NFT : NTFExchange offre une plateforme en ligne permettant aux utilisateurs d'acheter, vendre et échanger des NFT de manière sécurisée et transparente.</li>
+		<li>Vente aux enchères de NFT : L'entreprise organise des ventes aux enchères régulières où les utilisateurs peuvent proposer leurs NFT uniques et les collectionneurs peuvent enchérir pour les acquérir.</li>
+		<li>Galerie d'art numérique : NTFExchange peut présenter une galerie virtuelle où les artistes peuvent exposer leurs œuvres NFT, offrant ainsi une visibilité accrue aux créateurs émergents.</li>
+		<li>Services de vérification et d'authentification : L'entreprise peut offrir des services de vérification et d'authentification des NFT pour garantir l'originalité et l'unicité des actifs numériques proposés sur sa plateforme.</li>
+		<li>Consultation et conseil : NTFExchange peut fournir des services de consultation et de conseil aux artistes, aux collectionneurs et aux investisseurs intéressés par l'univers des NFT, les aidant ainsi à comprendre le marché et à prendre des décisions éclairées.</li>
+		<li>Événements et compétitions : L'entreprise peut organiser des événements spéciaux tels que des expositions virtuelles, des conférences, des webinaires, ainsi que des compétitions pour encourager la participation de la communauté des créateurs et des collectionneurs.</li>
+		<li>Programmes de mentorat : NTFExchange peut mettre en place des programmes de mentorat pour soutenir les artistes et les créateurs émergents, en les aidant à développer leurs compétences et à atteindre leur plein potentiel dans l'écosystème des NFT.
+</li>
+		<li>Plateforme de financement participatif : L'entreprise peut proposer une plateforme de financement participatif basée sur les NFT, permettant aux artistes de lever des fonds pour leurs projets créatifs en émettant des NFT uniques en tant que récompenses.
+</li>
+		<li>Édition limitée et collections spéciales : NTFExchange peut collaborer avec des artistes renommés pour créer des collections spéciales et des éditions limitées de NFT, offrant ainsi des opportunités exclusives aux collectionneurs.</li>
+		<li>Éducation et sensibilisation : L'entreprise peut développer du contenu éducatif et des ressources en ligne pour sensibiliser le public aux NFT, expliquer les concepts clés et promouvoir les bonnes pratiques dans le domaine.</li>
+		</ul>
+			
+	</div>";
+
+
+pagefooter();
+
+?>
---
+++ home.php
@@ -0,0 +1,31 @@
+<?php
+
+include "functions.php";
+
+setup("SAE23");
+
+
+
+pageheader('NTFExchange');
+
+pagenavbar();
+
+echo "<div class='container'>
+			<h1>Qui sommes nous ?</h1>
+			<p>NTFExchange est une entreprise de premier plan dans le domaine des NFT (Non-Fungible Tokens) et de l'échange de biens numériques uniques. Nous sommes une plateforme en ligne sécurisée qui permet aux utilisateurs d'acheter, de vendre et de échanger des NFT de manière transparente et efficace.
+
+Chez NTFExchange, nous comprenons le potentiel révolutionnaire des NFT dans le domaine de l'art, de la musique, des jeux et bien d'autres secteurs. Nous croyons fermement que les NFT sont la prochaine évolution de la propriété numérique, offrant aux créateurs et aux collectionneurs la possibilité de détenir des actifs numériques uniques et authentifiés.
+
+Notre plateforme offre une expérience conviviale et intuitive, permettant aux utilisateurs de naviguer facilement à travers une large gamme de NFT disponibles à l'achat. Que vous soyez un artiste émergent cherchant à exposer votre travail, un collectionneur passionné à la recherche de pièces rares ou un investisseur souhaitant participer à ce nouveau marché en plein essor, NTFExchange est là pour vous accompagner.
+
+Nous nous engageons à maintenir la sécurité de nos utilisateurs et de leurs actifs numériques. Notre système utilise des protocoles de cryptographie avancés pour garantir l'authenticité et la traçabilité des NFT échangés sur notre plateforme. De plus, nous collaborons avec des experts en sécurité informatique pour prévenir les risques de piratage et de fraude.
+
+NTFExchange est également un espace communautaire dynamique où les créateurs et les passionnés peuvent interagir, partager leurs œuvres, participer à des enchères et à des événements spéciaux. Nous encourageons l'échange d'idées, la collaboration et la découverte de nouvelles formes d'expression numérique.
+
+Rejoignez NTFExchange dès aujourd'hui et faites partie de la révolution des NFT. Découvrez des œuvres uniques, trouvez des opportunités d'investissement passionnantes et connectez-vous avec une communauté mondiale de créateurs et de collectionneurs. Ensemble, nous donnons vie à la propriété numérique.</p>
+	</div>";
+
+
+pagefooter();
+
+?>
