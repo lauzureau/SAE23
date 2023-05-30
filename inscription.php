31477e1c9f869e3b05bc3dfa961a1c4c2036e37c
hmanivel <133368554+hmanivel@users.noreply.github.com>
2023-05-30 19:21:57 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -14,14 +14,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $email = $_POST['email'];
   
-    // Vérifier si les champs sont vides
+     // Vérifier si les champs sont vides
     if (empty($username) || empty($password) || empty($email)) {
         $error = "Veuillez remplir tous les champs du formulaire.";
     } else {
-        // Traitement de l'inscription (ajoutez votre logique ici)
-        // ...
-        // Rediriger l'utilisateur vers une page de succès
-        header("Location: inscription_success.php?success=inscription");
+        // Récupérer les données existantes du fichier comptes.json
+        $comptes = json_decode(file_get_contents('./js/comptes.json'), true);
+
+        // Ajouter les nouvelles données du compte à $comptes
+        $nouveauCompte = [
+            'username' => $username,
+            'password' => $password,
+            'email' => $email
+        ];
+
+        $comptes[] = $nouveauCompte;
+
+        // Convertir le tableau $comptes en format JSON
+        $jsonComptes = json_encode($comptes, JSON_PRETTY_PRINT);
+
+        // Écrire le contenu JSON dans le fichier comptes.json
+        file_put_contents('./js/comptes.json', $jsonComptes);
+
+        // Rediriger l'utilisateur vers une page de succès avec le paramètre correspondant
+        header("Location: inscription_success.php?success=inscription_reussie");
         exit;
     }
 }
