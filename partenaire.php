fb212c57dbd792799a1c494bfc707dfe19853611
Gwen010 <134376447+Gwen010@users.noreply.github.com>
2023-06-03 17:57:06 +0200

    Update partenaire.php

--- partenaire.php
+++ partenaire.php
@@ -3,94 +3,183 @@ include "./functions.php";
 setup("Partenaire");
  ?>
 <!DOCTYPE html>
-<html>
-
+<html class="responsive">
 <?php pagenavbar();
-pageheader("Partenaire"); ?>
+pageheader("Nos partenaires"); ?>
 <body>
-
 <div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div id="Div" class="card">
-      <img onclick="Image()" src="../img/283.jpg" class="card-img-top" alt="Card image" style="width:100%">
-      <div class="card-body">
-        <h5  class="card-title">283</h5>
-        <p id="image" class="card-text">
-            Autheur : Mad Dog Jones
-
-</p>
-      </div>
-    </div>
-  </div>
-<style type="text/css">
-    div {
-  animation: mymove 5s linear alternate;
+   
+  <div class="col"> 
+    <style>
+        .container{
+            position: relative;
+            width: 50%;
+        }
+        .image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+        .text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+  white-space: nowrap;
+}
+.container:hover .overlay{
+  bottom: 0;
+  height: 100%;
+}
+.overlay {
+  position: absolute;
+  bottom: 100%;
+  left: 0;
+  right: 0;
+  background-color: grey;
+  overflow: hidden;
+  width: 100%;
+  height:0;
+  transition: .5s ease;
 }
 
-@keyframes mymove {
-  to {transform: rotateY(180deg);}
+    </style>
+     <div class="container">
+    <div class="card">
+
+         <h5  class="card-title">283</h5>
+      <img src="../img/283.jpg" class="image" alt="Card image" style="cursor:pointer">
+        <div class="overlay">  
+   <!-- <script> //Le script suivant permet d'afficher les infomartions quand on a cliquer sur l'image 283
+    document.getElementById("IMAGE293").addEventListener("click", image293);
+
+    function image293() {
+  document.getElementById("IMAGE293").innerHTML = ' <h5  class="card-title">283</h5><p id="image" class="card-text">Autheur : Mad Dog Jones</p>';
+    }
+  </script> -->
+   <div class="text">Autheur : Mad Dog Jones</div>
+        </div>
+    </div>
+    </div>
+  
+</div>
+
+  <div class="col">
+<style>
+.overlay3 {
+  position: absolute;
+  bottom: 0;
+  left: 100%;
+  right: 0;
+  background-color:grey;
+  overflow: hidden;
+  width: 0;
+  height: 100%;
+  transition: .5s ease;
+}
+
+.container:hover .overlay3 {
+  width: 100%;
+  left: 0;
+}
+
+</style>
+       <div class="container">
+    <div class="card">
+        <h5 class="card-title">The Vault</h5>
+      <img src="../img/the vault.gif" class="image" alt="Card image" style="cursor:pointer">
+      <div class="overlay3">
+         <div class="text">XCOPY <br>Platform: SuperRare  <br>Blockchain: Ethereum<br>Minted: 04/04/20<br><br>Type: Unique <br>Edition: 1/1 <br>Licence: CC0 <br>Owner: the_vault <br></div>
+    <!--  <script>
+document.getElementById("IMAGETheVault").addEventListener("click", imageTheVault);
+
+function imageTheVault() {
+  document.getElementById("IMAGETheVault").innerHTML = '<h5 class="card-title">The Vault</h5><p class="card-text">XCOPY <br>Platform: SuperRare  <br>Blockchain: Ethereum<br>Minted: 04/04/20<br><br>Type: Unique <br>Edition: 1/1 <br>Licence: CC0 <br>Owner: the_vault <br></p>';  }
+  </script> -->
+
+ </div>
+    </div>
+</div>
+</div>
+
+  <div class="col">
+    <style>
+.overlay2 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: grey;
+  overflow: hidden;
+  width: 0;
+  height: 100%;
+  transition: .5s ease;
+}
+
+.container:hover .overlay2 {
+  width: 100%;
 }
 </style>
-  <script>
-    function Image() {
-  let text = document.getElementById("image").innerHTML;
-  document.getElementById("image").innerHTML =
-  text.toUpperCase();
-}
-function myFunction(x) {
-  if (x.checked === true) {
-    document.getElementById("Div").style.backfaceVisibility = "visible";
-  }
-}
-  </script>
-  <div class="col">
+<div class="container">
     <div class="card">
-      <img id="image" src="../img/the vault.gif" class="card-img-top" alt="Card image" style="width:100%">
-      <div class="card-body">
-        <h5 class="card-title">The Vault</h5>
-        <p class="card-text">            XCOPY <br>
-Platform: SuperRare  <br>
-Blockchain: Ethereum<br>
-Minted: 04/04/20<br>
-<br>
-Type: Unique <br>
-Edition: 1/1 <br>
-Licence: CC0 <br>
-Owner: the_vault <br>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="../img/known origin.gif" class="card-img-top" alt="Card image">
-      <div class="card-body">
+      <img src="../img/known origin.gif" class="image" alt="Card image" style="cursor:pointer">
+      <div class="overlay2">
         <h5 class="card-title">Vault</h5>
-        <p class="card-text">Type: Edition <br>
-Edition: 1/100 <br>
-Licence: CC0 <br>
-Owner: ArtiVault <br>
-Platform: KnownOrigin<br>
-Minted: 21/10/18</p>
+        <div class="text">Type: Edition <br>
+    Edition: 1/100 <br>
+    Licence: CC0<br>
+    Owner: ArtiVault<br>
+    Platform: KnownOrigin<br>
+    Blockchain: Ethereum<br>
+    Minted: 21/10/18<br>
+ </div>
+
       </div>
     </div>
   </div>
+</div>
+
   <div class="col">
-    <div class="card">
-      <img src="../img/REPLICATOR.jpg" class="card-img-top" alt="Card image">
-      <div class="card-body">
-        <h4 class="card-title">Replicator</h4>
-        <p class="card-text">Autheur :
-Mad Dog Jones <br>
-Nom de l’œuvre REPLICATOR</p>
+    <style>
+.overlay4 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: grey;
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  transition: .5s ease;
+}
+
+.container:hover .overlay4 {
+  height: 100%;
+}
+
+</style>
+    <div class="container">
+    <div  class="card">
+      <img src="../img/REPLICATOR.jpg" class="image" alt="Card image" style="cursor:pointer">
+      <div class="overlay4">
+
+        <div class="text">
+
+    Autheur : Mad Dog Jones <br>Nom de l’œuvre REPLICATOR </div>
       </div>
     </div>
   </div>
+</div>
+</div>
+
 
 
 <?php  
 pagefooter();?>
 </body>
 </html>
-
-<?php  
-?>
