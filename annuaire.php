ec9cc1986d11a56d94120deda491b755110f6e18
hmanivel <133368554+hmanivel@users.noreply.github.com>
2023-06-08 23:14:02 +0200

    Create annuaire.php

---
+++ annuaire.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Annuaire</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    include "functions.php";
+    setup("SAE23");
+    pageheader('NTFExchange');
+    pagenavbar();
+    ?>
+    
+    <h1>Annuaire</h1>
+    <div>
+        <?php
+        function showAccounts($accounts) {
+            if (count($accounts) > 0) {
+                echo '<table>';
+                echo '<tr><th>Nom</th><th>Mot de passe</th><th>Email</th></tr>';
+                foreach ($accounts as $account) {
+                    echo '<tr>';
+                    echo '<td>' . $account['username'] . '</td>';
+                    echo '<td>' . $account['password'] . '</td>';
+                    echo '<td>' . $account['email'] . '</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+            } else {
+                echo '<p>Aucun compte trouvé.</p>';
+            }
+        }
+        
+        // Afficher la liste des comptes à partir du fichier JSON
+        $accounts = json_decode(file_get_contents('./js/comptes.json'), true);
+        showAccounts($accounts);
+        ?>
+    </div>
+    
+    <?php
+    pagefooter();
+    ?>
+</body>
+</html>
