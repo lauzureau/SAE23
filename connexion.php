a4dc9d978147e4b4bf300b450b4bf63dfdac5489
hmanivel <133368554+hmanivel@users.noreply.github.com>
2023-06-08 15:33:57 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@ $comptes = json_decode(file_get_contents('./js/comptes.json'), true);
 
 // Vérification du formulaire de connexion
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   // Récupérer les valeurs du formulaire
+    // Récupérer les valeurs du formulaire
     $username = isset($_POST['username']) ? $_POST['username'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
 
@@ -12,17 +12,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($username) || empty($password)) {
         $error = "Veuillez remplir tous les champs du formulaire.";
     } else {
-    // Vérifier les identifiants
-    foreach ($comptes as $compte) {
-        if ($compte['username'] === $username && $compte['password'] === $password) {
-            // Identifiants corrects, rediriger vers une page de succès
-            header("Location: inscription_success.php?type=connexion&username=" . urlencode($username));
-            exit;
+        // Vérifier les identifiants
+        foreach ($comptes as $compte) {
+            if ($compte['username'] === $username && $compte['password'] === $password) {
+                // Identifiants corrects, rediriger vers une page de succès
+                header("Location: inscription_success.php?type=connexion&username=" . urlencode($username));
+                exit;
+            }
         }
-    }
 
-    // Identifiants incorrects, afficher un message d'erreur
-    $error = "Identifiants incorrects. Veuillez réessayer.";
+        // Identifiants incorrects, afficher un message d'erreur
+        $error = "Identifiants incorrects. Veuillez réessayer.";
+    }
 }
 
 // Reste du code de la page de connexion
