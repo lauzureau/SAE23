bb1547723be4a97c55c839946beaa71d4949d930
lauzureau <133368444+lauzureau@users.noreply.github.com>
2023-06-09 11:09:53 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function pagenavbar(){
   <div class="container-fluid">
   <ul class="navbar-nav">
   <li class="nav-item">
-  <a class="nav-link" href="Accueil.php">Accueil</a>
+  <a class="nav-link" href="index.php">Accueil</a>
   </li>
   <li class="nav-item">
   <a class="nav-link" href="About.php">Qui sommes nous ?</a>
