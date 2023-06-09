57d85dfd82df74f5536ae9e24ff0d61bfba5709d
hmanivel <133368554+hmanivel@users.noreply.github.com>
2023-06-09 09:51:32 +0200

    Update Histoire.php

--- Histoire.php
+++ Histoire.php
@@ -2,126 +2,124 @@
 <html>
 <head>
   <style>
-    // {
-   -webkit-box-sizing: border-box;
-   -moz-box-sizing: border-box;
-   box-sizing: border-box;
-   outline: none;
-}
+    /* CSS styles for the timeline */
+    body {
+       margin: 0;
+       padding: 30px 0;
+       font-family: 'Roboto', sans-serif;
+       background: #F1F2F6;
+    }
 
-//body {
-   margin: 0;
-   padding: 30px 0;
-   font-family: 'Roboto', sans-serif;
-   background: #F1F2F6;
-}
+    h1 {
+       text-align: center;
+       font-weight: 300;
+       color: #777
+    }
 
-//h1 {
-   text-align: center;
-   font-weight: 300;
-   color: #777
-}
+    h1 span {
+       font-weight: 600;
+    }
 
-//h1 span {
-   font-weight: 600;
-}
+    .timeline-container {
+       width: 80%;
+       padding: 50px 0;
+       margin: 50px auto;
+       position: relative;
+       overflow: hidden;
+    }
 
-.timeline-container {
-   width: 80%;
-   padding: 50px 0;
-   margin: 50px auto;
-   position: relative;
-   overflow: hidden;
-}
-.timeline-container:before { /*This is the line down the middle*/
-   content: '';
-   position: absolute;
-   top: 0;
-   left: 50%;
-   margin-left: -1px;
-   width: 2px;
-   height: 100%;
-   background: #CCD1D9;
-   z-index: 1
-}
-.timeline-block {
-   width: -webkit-calc(50% + 8px);
-   width: -moz-calc(50% + 8px);
-   width: calc(50% + 8px);
-   display: -webkit-box;
-   display: -webkit-flex;
-   display: -moz-box;
-   display: flex;
-   -webkit-box-pack: justify;
-   -webkit-justify-content: space-between;
-   -moz-box-pack: justify;
-   justify-content: space-between;
-   clear: both;
-}
-.timeline-block-right {
-   float: right;
-}
-.timeline-block-left {
-   float: left;
-   direction: rtl
-}
-.marker {
-   width: 16px;
-   height: 16px;
-   border-radius: 50%;
-   border: 2px solid #F5F7FA;
-   background: #4DD0E1;
-   margin-top: 10px;
-   z-index: 9999
-}
-.timeline-content {
-   width: 95%;
-   padding: 0 15px;
-   color: #666
-}
-.timeline-content h3 {
-   margin-top: 5px;
-   margin-bottom: 5px;
-   font-size: 25px;
-   font-weight: 500
-}
-.timeline-content span {
-   font-size: 15px;
-   color: #a4a4a4;
-}
-.timeline-content p {
-   font-size: 14px;
-   line-height: 1.5em;
-   word-spacing: 1px;
-   color: #888;
-}
-@media screen and (max-width: 768px) {
-   .timeline-container:before {
-      left: 8px;
-      width: 2px;
-   }
-   .timeline-block {
-      width: 100%;
-      margin-bottom: 30px;
-   }
-   .timeline-block-right {
-      float: none;
-   }
-   .timeline-block-left {
-      float: none;
-      direction: ltr;
-   }
-}
+    .timeline-container:before { /*This is the line down the middle*/
+       content: '';
+       position: absolute;
+       top: 0;
+       left: 50%;
+       margin-left: -1px;
+       width: 2px;
+       height: 100%;
+       background: #CCD1D9;
+       z-index: 1
+    }
 
+    .timeline-block {
+       width: calc(50% + 8px);
+       display: flex;
+       justify-content: space-between;
+       clear: both;
+    }
+
+    .timeline-block-right {
+       float: right;
+    }
+
+    .timeline-block-left {
+       float: left;
+       direction: rtl
+    }
+
+    .marker {
+       width: 16px;
+       height: 16px;
+       border-radius: 50%;
+       border: 2px solid #F5F7FA;
+       background: #4DD0E1;
+       margin-top: 10px;
+       z-index: 9999
+    }
+
+    .timeline-content {
+       width: 95%;
+       padding: 0 15px;
+       color: #666
+    }
+
+    .timeline-content h3 {
+       margin-top: 5px;
+       margin-bottom: 5px;
+       font-size: 25px;
+       font-weight: 500
+    }
+
+    .timeline-content span {
+       font-size: 15px;
+       color: #a4a4a4;
+    }
+
+    .timeline-content p {
+       font-size: 14px;
+       line-height: 1.5em;
+       word-spacing: 1px;
+       color: #888;
+    }
+
+    @media screen and (max-width: 768px) {
+       .timeline-container:before {
+          left: 8px;
+          width: 2px;
+       }
+
+       .timeline-block {
+          width: 100%;
+          margin-bottom: 30px;
+       }
+
+       .timeline-block-right {
+          float: none;
+       }
+
+       .timeline-block-left {
+          float: none;
+          direction: ltr;
+       }
+    }
   </style>
 </head>
 <body>
 <?php
-    include "functions.php";
-    setup("SAE23");
-    pageheader('NTFExchange');
-    pagenavbar();
-    ?>
+include "functions.php";
+setup("SAE23");
+pageheader('NTFExchange');
+pagenavbar();
+?>
   <div class="timeline-container">
     <div class="timeline-block timeline-block-right">
       <div class="marker"></div>
@@ -155,8 +153,8 @@
       </div>
     </div>
   </div>
-  <?php
-    pagefooter();
-    ?>
+<?php
+pagefooter();
+?>
 </body>
 </html>
