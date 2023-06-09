fd4332f43a146e264cf9faaced852285399d1dd3
lauzureau <133368444+lauzureau@users.noreply.github.com>
2023-06-09 13:09:13 +0200

    Create Wiki.php

---
+++ Wiki.php
@@ -0,0 +1,22 @@
+<?php
+
+include "functions.php";
+
+setup("SAE23");
+
+echo ' <div class="container mt-3">';
+
+pageheader('NTFExchange');
+
+pagenavbar();
+
+echo '<h1 style="color:#ffc107;">Wiki</h1>';
+
+echo '<div class="mt-4 p-5 bg-dark text-warning rounded">';
+echo "<p>Notre site fonctionne principalement grâce à la page function.php qui regroupe toutes les fonctions permettant la mise en place de la navbarre, du footer ou encore du header.
+</p>
+	</div>";
+
+pagefooter();
+
+?>
