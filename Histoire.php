67f36ee4c0a37d440bd17120377cf1a56a761482
hmanivel <133368554+hmanivel@users.noreply.github.com>
2023-06-09 09:21:50 +0200

    Update Histoire.php

--- Histoire.php
+++ Histoire.php
@@ -1,126 +1,161 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Histoire</title>
-    <style>
-        .card-container {
-            perspective: 1000px;
-            width: 200px;
-            height: 260px;
-            position: relative;
-        }
-        
-        .card {
-            width: 100%;
-            height: 100%;
-            transition: transform 0.6s;
-            transform-style: preserve-3d;
-            cursor: pointer;
-        }
-        
-        .card.flipped {
-            transform: rotateY(180deg);
-        }
-        
-        .card-front,
-        .card-back {
-            width: 100%;
-            height: 100%;
-            position: absolute;
-            backface-visibility: hidden;
-        }
-        
-        .card-front {
-            background-color: #F8F8F8;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-        
-        .card-back {
-            background-color: #E0E0E0;
-            transform: rotateY(180deg);
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-        }
+  <style>
+    // {
+   -webkit-box-sizing: border-box;
+   -moz-box-sizing: border-box;
+   box-sizing: border-box;
+   outline: none;
+}
 
-        .center-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            flex-direction: column; /* Ajout de cette ligne */
-        }
-    </style>
-    <script>
-        function flipCard(element) {
-            element.classList.toggle("flipped");
-        }
-    </script>
+//body {
+   margin: 0;
+   padding: 30px 0;
+   font-family: 'Roboto', sans-serif;
+   background: #F1F2F6;
+}
+
+//h1 {
+   text-align: center;
+   font-weight: 300;
+   color: #777
+}
+
+//h1 span {
+   font-weight: 600;
+}
+
+.timeline-container {
+   width: 80%;
+   padding: 50px 0;
+   margin: 50px auto;
+   position: relative;
+   overflow: hidden;
+}
+.timeline-container:before { /*This is the line down the middle*/
+   content: '';
+   position: absolute;
+   top: 0;
+   left: 50%;
+   margin-left: -1px;
+   width: 2px;
+   height: 100%;
+   background: #CCD1D9;
+   z-index: 1
+}
+.timeline-block {
+   width: -webkit-calc(50% + 8px);
+   width: -moz-calc(50% + 8px);
+   width: calc(50% + 8px);
+   display: -webkit-box;
+   display: -webkit-flex;
+   display: -moz-box;
+   display: flex;
+   -webkit-box-pack: justify;
+   -webkit-justify-content: space-between;
+   -moz-box-pack: justify;
+   justify-content: space-between;
+   clear: both;
+}
+.timeline-block-right {
+   float: right;
+}
+.timeline-block-left {
+   float: left;
+   direction: rtl
+}
+.marker {
+   width: 16px;
+   height: 16px;
+   border-radius: 50%;
+   border: 2px solid #F5F7FA;
+   background: #4DD0E1;
+   margin-top: 10px;
+   z-index: 9999
+}
+.timeline-content {
+   width: 95%;
+   padding: 0 15px;
+   color: #666
+}
+.timeline-content h3 {
+   margin-top: 5px;
+   margin-bottom: 5px;
+   font-size: 25px;
+   font-weight: 500
+}
+.timeline-content span {
+   font-size: 15px;
+   color: #a4a4a4;
+}
+.timeline-content p {
+   font-size: 14px;
+   line-height: 1.5em;
+   word-spacing: 1px;
+   color: #888;
+}
+@media screen and (max-width: 768px) {
+   .timeline-container:before {
+      left: 8px;
+      width: 2px;
+   }
+   .timeline-block {
+      width: 100%;
+      margin-bottom: 30px;
+   }
+   .timeline-block-right {
+      float: none;
+   }
+   .timeline-block-left {
+      float: none;
+      direction: ltr;
+   }
+}
+
+  </style>
 </head>
 <body>
-    <?php
+<?php
     include "functions.php";
     setup("SAE23");
-    
-    echo '<div class="container mt-3">';
     pageheader('NTFExchange');
     pagenavbar();
-    
-    echo '<div class="center-container">'; // Container to center the card section
-    
-    echo '<div class="card-container">';
-    
-    // Timeline item 1
-    echo '<div class="card" onclick="flipCard(this)">';
-    echo '<div class="card-front">';
-    echo '<h3>2017</h3>';
-    echo '</div>';
-    echo '<div class="card-back">';
-    echo '<h3>2017</h3>';
-    echo '<p>Fondation de NTFExchange par un groupe d\'entrepreneurs passionnés par le potentiel des NFTs dans le domaine de l\'art numérique.</p>';
-    echo '</div>';
-    echo '</div>';
-    
-    // Timeline item 2
-    echo '<div class="card" onclick="flipCard(this)">';
-    echo '<div class="card-front">';
-    echo '<h3> 2018</h3>';
-    echo '</div>';
-    echo '<div class="card-back">';
-    echo '<h3>2018</h3>';
-    echo '<p>Sous la présidence de John Davis, NTFExchange connaît une croissance significative et devient une plateforme réputée pour l\'achat, la vente et l\'échange de NFTs. L\'entreprise établit des partenariats stratégiques avec des artistes et des collectionneurs influents.</p>';
-    echo '</div>';
-    echo '</div>';
-    
-    // Timeline item 3
-    echo '<div class="card" onclick="flipCard(this)">';
-    echo '<div class="card-front">';
-    echo '<h3> 2021</h3>';
-    echo '</div>';
-    echo '<div class="card-back">';
-    echo '<h3> 2021</h3>';
-    echo '<p>NTFExchange lance sa propre plateforme de création et de vente de NFTs, offrant ainsi une solution complète pour les artistes souhaitant se lancer dans l\'univers des NFTs.</p>';
-    echo '</div>';
-    echo '</div>';
-    
-    // Timeline item 4
-    echo '<div class="card" onclick="flipCard(this)">';
-    echo '<div class="card-front">';
-    echo '<h3> 2023</h3>';
-    echo '</div>';
-    echo '<div class="card-back">';
-    echo '<h3>2023</h3>';
-    echo '<p>NTFExchange franchit le cap des 10 millions de NFTs vendus sur sa plateforme, devenant ainsi l\'une des principales références du marché des NFTs.</p>';
-    echo '</div>';
-    echo '</div>';
-    
-    echo '</div>'; // .card-container
+    ?>
+  <div class="timeline-container">
+    <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+        <h3>2017</h3>
+        <p>Fondation de NTFExchange par un groupe d'entrepreneurs passionnés par le potentiel des NFTs dans le domaine de l'art numérique.</p>
+      </div>
+    </div>
 
-    echo '</div>'; // .center-container
-    
+    <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+        <h3>2018</h3>
+        <p>Sous la présidence de John Davis, NTFExchange connaît une croissance significative et devient une plateforme réputée pour l'achat, la vente et l'échange de NFTs. L'entreprise établit des partenariats stratégiques avec des artistes et des collectionneurs influents.</p>
+      </div>
+    </div>
+
+    <div class="timeline-block timeline-block-right">
+      <div class="marker"></div>
+      <div class="timeline-content">
+        <h3>2021</h3>
+        <p>NTFExchange lance sa propre plateforme de création et de vente de NFTs, offrant ainsi une solution complète pour les artistes souhaitant se lancer dans l'univers des NFTs.</p>
+      </div>
+    </div>
+
+    <div class="timeline-block timeline-block-left">
+      <div class="marker"></div>
+      <div class="timeline-content">
+        <h3>2023</h3>
+        <p>NTFExchange franchit le cap des 10 millions de NFTs vendus sur sa plateforme, devenant ainsi l'une des principales références du marché des NFTs.</p>
+      </div>
+    </div>
+  </div>
+  <?php
     pagefooter();
     ?>
 </body>
